768c316fcb207b23fcc9acfb0ab0b461b46a8563
jinyoung <sm099@naver.com>
2022-08-31 01:55:54 +0900

    Refactor : delete email in payload

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -49,7 +49,7 @@ export function verifyRefreshToken(token : string) {
         // 인자로 받은 token이 유효한지 확인하는 변수 (유효하다면 decoded가 존재)
         const decoded = jwt.verify(token, PUBLIC_KEY) as jwt.JwtPayload;
         // 유효하다면 payload에 decoded를 넣고 expired에 false로 리턴(만료되지 x)
-        return { payload : {email : decoded.email, sessionId : decoded.sessionId}, expired : false };
+        return { payload : {sessionId : decoded.sessionId}, expired : false };
     } catch (error : any) {
         // 만약 유효하지 않다면 payload는 Null, expired엔 errorMessage를 담아 리턴
         return { expired: error.message.includes("jwt expired")};
