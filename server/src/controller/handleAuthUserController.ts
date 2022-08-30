8b3532035322e4821b6ec00ae36c60bce6805ffc
jinyoung <sm099@naver.com>
2022-08-30 18:12:17 +0900

    Refactor : auth user api response modify

--- server/src/controller/handleAuthUserController.ts
+++ server/src/controller/handleAuthUserController.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from "express";
-import { verifyJWT } from "../utils/jwt";
+import { verifyAccessToken } from "../utils/jwt";
 
 export default function handleAuthUser(req : Request, res : Response) {
     let token = req.headers.authorization?.split(' ').slice(1).join() as string;
 
-    let verifyToken = verifyJWT(token);
+    let verifyToken = verifyAccessToken(token);
 
     if(verifyToken.expired === false) {
         return res.status(200).send({
-            "ok" : 200,
+            "status" : 200,
             "message" : "valid token",
-            "payload" : verifyToken.payload
+            "email" : verifyToken.payload
         });
     } else {
         return res.status(401).send({
-            "ok" : 401,
+            "status" : 401,
             "message" : "invalid token"
         })
     }
