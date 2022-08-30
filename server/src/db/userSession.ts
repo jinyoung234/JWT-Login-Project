0e3d996c408b8640a18fc9b3221b808fdf513c1e
jinyoung <sm099@naver.com>
2022-08-31 01:55:13 +0900

    add getSession logic

--- server/src/db/userSession.ts
+++ server/src/db/userSession.ts
@@ -1,10 +1,16 @@
-interface IUserSession {
+export interface IUserSession {
     sessionId : string;
     email : string;
     valid : boolean
 }
 
 export const userSession : Record<string, IUserSession> = {};
+
+export function getSession(sessionId : string) {
+    const sessionKey = Object.keys(userSession).find(id => id === sessionId) as string;
+    
+    return userSession[sessionKey];
+}
 
 // 유저의 세션 생성
 export function createSession(email : string) {
