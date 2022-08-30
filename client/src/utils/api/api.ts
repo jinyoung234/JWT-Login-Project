1f100165d4a50181b297c3ce7d5ea837dbbd24aa
jinyoung <sm099@naver.com>
2022-08-31 02:02:16 +0900

    feat : baseURL and withCredentials settings and refresh api implement

--- client/src/utils/api/api.ts
+++ client/src/utils/api/api.ts
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+export const client = axios.create({
+    baseURL : "http://localhost:4000",
+    withCredentials : true,
+})
+
+export async function refreshApi() {
+    return await client.get("/api/refresh").then(
+        res => {client.defaults.headers.common['Authorization'] = `Bearer ${res.data.accessToken}`;
+    })
+}
+
