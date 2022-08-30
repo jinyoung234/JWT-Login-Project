8b08c3c5b37c6555a2e6bba082890034bb66c852
jinyoung <sm099@naver.com>
2022-08-30 17:23:05 +0900

    Refactor : jwt verify logic modify

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -35,11 +35,11 @@ export function signJWT(payload : object, expiresIn : string | number) {
 export function verifyJWT(token : string) {
     try {
         // 인자로 받은 token이 유효한지 확인하는 변수 (유효하다면 decoded가 존재)
-        const decoded = jwt.verify(token, PUBLIC_KEY);
+        const decoded = jwt.verify(token, PUBLIC_KEY) as jwt.JwtPayload;
         // 유효하다면 payload에 decoded를 넣고 expired에 false로 리턴(만료되지 x)
-        return { payload : decoded, expired : false };
+        return { payload : decoded.email, expired : false };
     } catch (error : any) {
         // 만약 유효하지 않다면 payload는 Null, expired엔 errorMessage를 담아 리턴
-        return { payload : null, expired: error.message.includes("jwt expired")};
+        return { expired: error.message.includes("jwt expired")};
     }
 }
