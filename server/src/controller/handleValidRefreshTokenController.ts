f60911425691ace9ceff0b4a3cd0d969c6683a55
jinyoung <sm099@naver.com>
2022-08-31 01:58:16 +0900

    Refactor : replace session data in accessToken sign

--- server/src/controller/handleValidRefreshTokenController.ts
+++ server/src/controller/handleValidRefreshTokenController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { getSession, IUserSession } from "../db/userSession";
 import { signJWT, verifyRefreshToken } from "../utils/jwt";
 
 export default function handleValidRefreshToken(req : Request, res : Response) {
@@ -6,15 +7,20 @@ export default function handleValidRefreshToken(req : Request, res : Response) {
     let isVaildRefreshToken = verifyRefreshToken(refreshToken as string);
 
     if(isVaildRefreshToken.expired === false) {
+        const SESSION_ID = isVaildRefreshToken.payload?.sessionId;
+
+        const SESSION = getSession(SESSION_ID) as IUserSession;
         const accessToken = signJWT({
-            email : isVaildRefreshToken.payload?.email, 
-            sessionId : isVaildRefreshToken.payload?.sessionId
+            email : SESSION.email,
+            sessionId : SESSION_ID
         }, "5s")
+
         return res.status(200).send({
             "status" : 200,
             "message" : "재발급이 완료되었습니다.",
             "accessToken" : accessToken
         });
+
     } else {
         return res.status(401).send({
             "status" : 401,
