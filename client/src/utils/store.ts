12ad3d1af5640227d638338b043a90913c9202e2
jinyoung <sm099@naver.com>
2022-09-02 15:54:56 +0900

    Refactor : seperate api logic in selector

--- client/src/utils/store.ts
+++ client/src/utils/store.ts
@@ -1,1 +1,39 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { client } from "./api/api";
+
+interface IToken {
+    status: string;
+    message : string;
+    accessToken : string;
+}
+
+interface IResponse {
+    status: string;
+    message : string;
+    email : string;
+}
+
+export const userInfo = atom({
+    key: 'userInfo',
+    default : ''
+})
+export const refreshSelector = selector({
+    key : 'refresh/user',
+    get : async ({get}) => {
+        let user = get(userInfo);
+        await client.get('/api/refresh')
+        .then( async (res) => {
+            const data = res.data as IToken;
+            client.defaults.headers.common["Authorization"] = `Bearer ${data.accessToken}`
+            await client.get("/api/user")
+            .then(res => {
+                let data = res.data as IResponse
+                user = data["email"];
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        });
+        return user
+    }
+})
